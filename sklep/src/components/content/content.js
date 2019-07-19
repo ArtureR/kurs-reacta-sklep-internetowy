275a7020ae56f80645e521c7e8fb1c0050125418
Tomasz <tomaszgralak94@gmail.com>
2019-07-19 18:22:45 +0200

    added not found scenario

--- sklep/src/components/content/content.js
+++ sklep/src/components/content/content.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ProductsList from '../productsList/productsList';
 
-const Content = (props) => {
-    return (
-        <div className="content">
-        <ProductsList products={props.products} addToCart={props.addToCart} />
-        </div>
-    );
+class Content extends React.Component {
+    render() {
+        const { products, addToCart } = this.props;
+        const isSearchEmpty = products.length === 0;
+        return (
+            <div className="content">
+                {isSearchEmpty ? (
+                    <p>item not found</p>
+                ) : (
+                    <ProductsList products={products} addToCart={addToCart} />
+                )}
+            </div>
+        );
+    }
 };
 
 export default Content;
