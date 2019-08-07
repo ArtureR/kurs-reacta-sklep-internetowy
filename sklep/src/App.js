afe48b8e8164f2af65d7b63372ee8996c1282c6d
Asia <jczyzo@gmail.com>
2019-08-07 23:05:25 +0200

    Router add package and simple routing

--- sklep/src/App.js
+++ sklep/src/App.js
@@ -1,38 +1,50 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import Sidebar from "react-sidebar";
 import Header from './components/header/header';
 import Content from './components/content/content';
 import CartSidebar from './components/cartSidebar/cartSidebar';
 import { connect } from "react-redux";
 import { toggleSidebar } from "./store/actions/actions";
+import About from './components/about/about';
 
 class App extends Component {
   render() {
     const { sidebarOpen, toggleSidebar } = this.props;
 
     return (
-      <div className="App">
-        <Sidebar
-          sidebar={<CartSidebar/>}
-          open={sidebarOpen}
-          onSetOpen={() => toggleSidebar()}
-          pullRight={true}
-          shadow={true}
-          overlayClassName={"sidebar-overlay"}
-          // use this style in case of toggle sidebar shadow
-          // overlay: { background: "rgba(0, 0, 0, 0.0)" },
-          styles={{
-            sidebar: {
-              background: "white",
-              top: "62px",
-              zIndex: 3
-            },
-          }}
-        >
-          <Header />
-          <Content />
-        </Sidebar>
-      </div>
+      <Router>
+        <div className="App">
+          <Sidebar
+            sidebar={<CartSidebar />}
+            open={sidebarOpen}
+            onSetOpen={() => toggleSidebar()}
+            pullRight={true}
+            shadow={true}
+            overlayClassName={"sidebar-overlay"}
+            // use this style in case of toggle sidebar shadow
+            // overlay: { background: "rgba(0, 0, 0, 0.0)" },
+            styles={{
+              sidebar: {
+                background: "white",
+                top: "62px",
+                zIndex: 3
+              },
+            }}
+          >
+            <Header />
+            <Route path="/about" component={About} />
+            <Route
+              exact
+              path="/"
+              render={props => <Content {...props} />}
+            />
+          </Sidebar>
+        </div>
+      </Router>
     );
   }
 }
@@ -41,7 +53,7 @@ const mapDispatchToProps = {
   toggleSidebar
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return { sidebarOpen: state.sidebarOpen };
 };
 
