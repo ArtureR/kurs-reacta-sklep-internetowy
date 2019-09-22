d2ea260320f9ef1d0e1a5662056297c1335c3526
Asia <jczyzo@gmail.com>
2019-09-23 00:23:44 +0200

    Add carusel for product view and link to products from productListItem

--- sklep/src/components/productsListItem/productsListItem.js
+++ sklep/src/components/productsListItem/productsListItem.js
@@ -1,23 +1,32 @@
 import React from "react";
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import AddToCart from '../addToCart/addToCart';
+import { Link } from 'react-router-dom';
 
 const ProductsListItem = (props) => {
     const { product } = props;
 
     return (
         <Card>
-            <Card.Img
-                variant="top"
-                src={`${process.env.PUBLIC_URL}/photo/${product.photo}`}
-                alt={product.name}
-                className="products-list-item-photo"
-            />
+            <Link to={`/product/${product.id}`}>
+                <Card.Img
+                    variant="top"
+                    src={`${process.env.PUBLIC_URL}/photo/${product.photo}`}
+                    alt={product.name}
+                    className="products-list-item-photo"
+                />
+            </Link>
             <Card.Body>
                 <Card.Title className="products-list-item-title">{product.name}</Card.Title>
                 <h6 className="products-list-item-price card-subtitle mb-2 text-muted">{product.price} zł</h6>
                 <Card.Text className="products-list-item-description">{product.description}</Card.Text>
                 <AddToCart productId={product.id} />
+                <Button
+                    variant="secondary"
+                    as={Link}
+                    to={`/product/${product.id}`}
+                    className="pl-4 pr-4 mt-2"
+                >Show more</Button>
             </Card.Body>
         </Card>
     );
--- sklep/src/views/product/product.js
+++ sklep/src/views/product/product.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { connect } from "react-redux";
 import { getProducts } from "../../store/selectors/selectors";
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Carousel } from 'react-bootstrap';
 import AddToCart from '../../components/addToCart/addToCart';
 
 const Product = (props) => {
   const currentId = parseInt(props.match.params.id);
   const { products } = props;
   const currentProduct = products.find(product => product.id === currentId);
+  const hasPhotosList = currentProduct.photos && currentProduct.photos.length > 0;
+  const photosToDisplay = hasPhotosList && currentProduct.photos.map((photo, index) => {
+    return (
+      <Carousel.Item key={index}>
+        <img
+          className="d-block w-100"
+          src={`${process.env.PUBLIC_URL}/photo/${photo}`}
+          alt="First slide"
+        />
+      </Carousel.Item>
+    );
+  });
 
   return (
     <Container className="product" >
       <Row>
         <Col xs={12} md={7}>
-          <Image src={`${process.env.PUBLIC_URL}/photo/${currentProduct.photo}`} fluid />
+          {hasPhotosList ? (
+            <Carousel>
+              {photosToDisplay}
+            </Carousel>
+          ) : (
+              <Image src={`${process.env.PUBLIC_URL}/photo/${currentProduct.photo}`} fluid />
+            )}
         </Col>
         <Col xs={12} md={5}>
           <h1 className="category-title">{currentProduct.name}</h1>
